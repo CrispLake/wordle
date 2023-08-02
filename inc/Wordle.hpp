fc4e0162a38aa1b85d9aef1f8bdbd6b07ace53a8
Eemil Majuri <eemilmajuri@gmail.com>
2023-08-02 15:16:16 +0300

    Added color codes and a header

--- inc/Wordle.hpp
+++ inc/Wordle.hpp
@@ -6,12 +6,17 @@
 /*   By: emajuri <emajuri@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 12:59:31 by emajuri           #+#    #+#             */
-/*   Updated: 2023/08/02 13:47:01 by emajuri          ###   ########.fr       */
+/*   Updated: 2023/08/02 15:08:30 by emajuri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WORDLE_HPP
 #define WORDLE_HPP
+
+#define RED "\e[0;31m"
+#define GRN "\e[0;32m"
+#define YEL "\e[0;33m"
+#define WHT "\e[0;37m"
 
 #include <string>
 
@@ -25,8 +30,9 @@ class Wordle {
 		std::string	words[6];
 		std::string	correct;
 
-		void	display();
+		void		display();
 		std::string	getWord();
+		void		printColor(char c, int index);
 };
 
 #endif
